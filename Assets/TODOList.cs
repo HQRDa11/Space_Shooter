b30930f88c92692cae16fee0ccae0a213e83cfdd
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-06-11 15:49:01 +0200

    Update TODOList.cs

--- Assets/TODOList.cs
+++ Assets/TODOList.cs
@@ -14,3 +14,24 @@ namespace Assets.Scripts.System
         // + Pattern toussa toussa
     }
 }
+
+namespace HQRDa
+{
+    public class TODOList
+    {
+
+        //TO DO:
+        // ok - importer rarete materials and enum
+        // ok - give bonus rarity
+        // ok - resoudre pb couleurs 
+        // -Create Turret Factory =>  construct turret with rarity
+        // - modify turret system to have AddTurret(Turret) fnctn
+        // - OnBonus: AddTurret(new Turret...)
+
+        // - augmenter degats;
+        // -verifier si le level/la vie des enemy est bien random mtn (isBug?)
+
+        // - Enemy shot
+        // - panel de loot sur la vague et pointeur enemy vers panel.
+    }
+}
