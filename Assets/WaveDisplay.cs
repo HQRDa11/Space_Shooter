c32e04ec1792ece2995d3ee700cb699a48c12439
NQSH <76408315+NQSH@users.noreply.github.com>
2021-06-11 12:38:19 +0200

    Add WaveDisplay

---
+++ Assets/WaveDisplay.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveDisplay : MonoBehaviour
+{
+    private static WaveDisplay _instance; public static WaveDisplay Instance { get => _instance; }
+    private Text _text;
+    private RectTransform _rectTransform;
+
+    private float _currentCombo;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _rectTransform = GetComponent<RectTransform>();
+        _currentCombo = 1;
+    }
+
+    private void Update()
+    {
+        ShowWaveIndex();
+    }
+
+    void FixedUpdate()
+    {
+        PulseEffectRevert();
+    }
+    private void ShowWaveIndex()
+    {
+        _text.text = WaveSystem.Instance.CurrentWaveIndex.ToString();
+    }
+
+    public void PulseEffect() { if (_text.fontSize < 110) _text.fontSize += 50; }
+    private void PulseEffectRevert() { if (_text.fontSize != 70) _text.fontSize -= 1; }
+}
