ff781c4efbbf09bb8df1e6103d19ac0e66c777ea
NQSH <76408315+NQSH@users.noreply.github.com>
2021-06-11 15:51:44 +0200

    Update TODOList.cs

--- Assets/TODOList.cs
+++ Assets/TODOList.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-namespace Assets.Scripts.System
+namespace NQSH
 {
-    public class TODOList : ScriptableObject
+    public class TODOList
     {
-        // Perte de combo avec le temps
+        // DONE - Perte de combo avec le temps
         // Perte de combo quand touché par enemy
 
         // Tire enemy avec balance de frequence
