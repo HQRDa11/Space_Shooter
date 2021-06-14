aa83364373c54affd42da97eede9cafc62ee9773
NQSH <76408315+NQSH@users.noreply.github.com>
2021-06-14 18:49:33 +0200

    Update Enemy.cs

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     public void Initialise(float level)
     {
         float health = 10 + level * 3;
         this.gameObject.GetComponent<Enemy_Health>().SetHealth(health, health);
+        this.gameObject.GetComponent<Enemy_Weapon>().SetShotChance(EnemyBalance.ShotChanceBalancing(20));
     }
 
 }
