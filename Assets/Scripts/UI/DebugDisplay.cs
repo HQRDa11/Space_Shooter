0cb914488295bb7684044c4095c73f6dbe743853
NQSH <76408315+NQSH@users.noreply.github.com>
2021-06-18 13:22:27 +0200

    Update DebugDisplay.cs

--- Assets/Scripts/UI/DebugDisplay.cs
+++ Assets/Scripts/UI/DebugDisplay.cs
@@ -1,5 +1,7 @@
-
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using System.Linq;
 
 public class DebugDisplay : MonoBehaviour
 {
@@ -18,5 +20,51 @@ public class DebugDisplay : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(Map.CheckPointIndexToPosition(i), .5f);
         }
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.G)) OrderBy(RandomArrayOfInt());
+    }
+
+    public int[] RandomArrayOfInt()
+    {
+        int[] intArray = new int[Random.Range(10, 50)];
+        for(int i = 0; i < intArray.Length; i++)
+        {
+            intArray[i] = Random.Range(0, 30);
+        }
+
+        return intArray;
+    }
+    public void OrderBy(int[] intArray)
+    {
+        List<int> list = new List<int>();
+        list = intArray.ToList();
+
+
+        List<int> orderedList = new List<int>();
+
+        while(list.Count > 0)
+        {
+            orderedList.Add(list.Min());
+            list.Remove(list.Min());
+        }
+
+        int[] arrayToReturn = new int[orderedList.Count];
+
+        for(int i = 0; i < orderedList.Count; i++)
+        {
+            if (i == 0) arrayToReturn[0] = orderedList[0];
+
+            else
+            {
+                if (i % 2 == 0) arrayToReturn[0 + i / 2] = orderedList[i];
+                else arrayToReturn[arrayToReturn.Length - 1 - i / 2] = orderedList[i];
+            }
+        }
+
+        foreach (int i in arrayToReturn) Debug.Log(i);
     }
 }
