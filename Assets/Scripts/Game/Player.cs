0cbb05345176e58f23a0a77f0150800ceb49b97e
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-06-21 15:33:46 +0200

    Update Player.cs

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -4,24 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Ship ship; // not done yet
+
+    // Ally system
+    private List<Ally> m_allAllies;
+    private int m_maxAllies;
+
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.GetComponent<TurretSystem>().Initialise();
+
+        m_maxAllies = 4;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T) == true)
-        {
-            this.gameObject.GetComponent<TurretSystem>().AddTurret(Factory.Instance.Turret_Factory.CreateRandom());
-        }
+        //if (Input.GetKeyDown(KeyCode.T) == true)
+        //{
+        //    this.gameObject.GetComponent<TurretSystem>().AddTurret(Factory.Instance.Turret_Factory.CreateRandom());
+        //}
     }
 
-    public void OnBonus(Rarity rarity)
+    public void OnTurretBonus(Rarity rarity)
     {
         Debug.Log("bonus on");
         this.gameObject.GetComponent<TurretSystem>().AddTurret(Factory.Instance.Turret_Factory.CreateTurret(rarity));
+    }
+    public void OnPilotBonus()
+    {
+        Debug.LogWarning("Pilot Bonus : not implemented yet");
     }
 }
