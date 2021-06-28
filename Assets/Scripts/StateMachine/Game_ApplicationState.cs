f3811f234f4bf102972b8d5ed844858e951e0b1c
NQSH <76408315+NQSH@users.noreply.github.com>
2021-06-28 21:03:39 +0200

    Tempo Score in EndGame

--- Assets/Scripts/StateMachine/Game_ApplicationState.cs
+++ Assets/Scripts/StateMachine/Game_ApplicationState.cs
@@ -25,6 +25,9 @@ public class Game_ApplicationState : ApplicationState
     public override void end()
     {
         // Call Game.save()
+        GameObject scoreParent = new GameObject("Score");
+        GameObject scoreChild = new GameObject(ScoreSystem.Instance.CurrentScore.ToString());
+        scoreChild.transform.parent = scoreParent.transform;
         GameObject.Destroy(m_game);
     }
 
