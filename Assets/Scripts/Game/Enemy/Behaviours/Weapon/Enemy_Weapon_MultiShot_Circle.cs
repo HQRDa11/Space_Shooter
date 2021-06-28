e4e8aefdc5b67e5f19a16240ce01b831c5bef234
NQSH <76408315+NQSH@users.noreply.github.com>
2021-06-28 22:53:11 +0200

    Add new Weapon Behaviour

---
+++ Assets/Scripts/Game/Enemy/Behaviours/Weapon/Enemy_Weapon_MultiShot_Circle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Weapon_MultiShot_Circle : Enemy_Behaviours.Weapon
+{
+    private bool _isFiring = false;
+    private int _numberOfShot = 20;
+    private int _shotIndex = 0;
+    public void Shoot(Enemy enemy)
+    {
+        if (_isFiring == false) _isFiring = true;
+
+        if (_isFiring == true)
+        {
+            float angle = enemy.transform.eulerAngles.z + (360 / _numberOfShot * _shotIndex); 
+            Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)); Debug.Log(direction);
+            GameObject shot = Factory.Instance.Shot_Factory.CreateShot(Factory.Instance.InGameObjectsList, Rarity.WHITE, direction, 3f, "Enemy");
+            shot.transform.position = enemy.transform.position;
+
+            if (_shotIndex < _numberOfShot)
+            {
+                _shotIndex++;
+                Shoot(enemy);
+            }
+            else
+            {
+                _isFiring = false;
+                _shotIndex = 0;
+            }
+        }
+    }
+}
