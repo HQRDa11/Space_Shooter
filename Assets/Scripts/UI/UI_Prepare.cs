8d24704d3c9a9ea7966e80cf5acf8a9e5042b860
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-16 09:12:05 +0200

    Update UI_Prepare.cs

--- Assets/Scripts/UI/UI_Prepare.cs
+++ Assets/Scripts/UI/UI_Prepare.cs
@@ -7,8 +7,9 @@ public class UI_Prepare : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ButtonsPanel newPanel = new ButtonsPanel(this.gameObject, "PREPARE NEXT FLIGHT", 64, new string[] { "GO !", "MainMenu" }, new Color32(32, 32, 32, 255), Color.white);
+        ButtonsPanel newPanel = new ButtonsPanel(this.gameObject, "PREPARE NEXT FLIGHT", 64, new string[] { " LAUNCH ASSAULT ", " UPGRADE SQUADRON "," BACK " }, new Color32(32, 32, 32, 255), Color.white);
         newPanel.Buttons[0].onClick.AddListener(() => Application_StateMachine.Instance.stateRequest(ApplicationState_Type.GAME));
-        newPanel.Buttons[1].onClick.AddListener(() => Application_StateMachine.Instance.stateRequest(ApplicationState_Type.MAINMENU));
+        newPanel.Buttons[1].onClick.AddListener(() => Application_StateMachine.Instance.stateRequest(ApplicationState_Type.UPGRADE));
+        newPanel.Buttons[2].onClick.AddListener(() => Application_StateMachine.Instance.stateRequest(ApplicationState_Type.MAINMENU));
     }
 }
