08c5f8ba9c811ee91c9a1d5e36e91343446dfef1
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-18 10:19:33 +0200

    Update TODOList.cs

--- Assets/Scripts/TODOList.cs
+++ Assets/Scripts/TODOList.cs
@@ -1,22 +1,23 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-namespace NQSH
-{
-    public class TODOList
-    {
-        // DONE - Perte de combo avec le temps
-        // Perte de combo quand touché par enemy
-
-        // Tire enemy avec balance de frequence
-
-        // Create BossWave class
-        // + Pattern toussa toussa
-    }
-}
-
 namespace HQRDa
 {
+
+    namespace NQSH
+    {
+        public class TODOList
+        {
+            // DONE - Perte de combo avec le temps
+            // Perte de combo quand touché par enemy
+
+            // Tire enemy avec balance de frequence
+
+            // Create BossWave class
+            // + Pattern toussa toussa
+        }
+    }
+
     public class TODOList
     {
         //TO DO:
@@ -28,16 +29,16 @@ namespace HQRDa
         // ok - Create Profile.ResetProfile();
         // ok - Profile Handler.cs: Profile NewProfile() : is launched if no valid latest save
         // ok - SquadronData is now save in profile correctly;
+        // ok - Update : UI_Update.cs
+        //    ok - Button : back to mainMenu
+        //    ok - Button template : switch states(previous/next)
+        //       ok - Next/Previous States Mechanics
+        //    ok - Fonctionnal Squadron Display 
+        //    ok - Functionnal Member Display
+        //    ok - Functionnal Module Display
+        // ok - Tools new fctn: int NewIndex( bool isNext_ifNotPrevious, int currentIndex) 
         //
-        // now - Update : UI_Update.cs
-        //          ok - Button : back to mainMenu
-        //                  ok - Button template : switch states(previous/next)
-        //          now - Fonctionnal Squadron Display 
-        //                  ok - Functionnal MemberDisplay
-        //                      ok - Correctly display indexed Member
-        //                      ok - Next/Previous Mechanics:
-        //                              ok - SquadronData: new fctn: int NewIndex( bool isNext_ifNotPrevious, int currentIndex)  
-        //                     - Functionnal ModuleDisplay
+        // now - Ship/Module Update Mechanics.
         //
         // - Create a Reset Profile button in MainMenu
         // delayed - (need sprites) - UI_Game.cs : image + Text pop-Up On bonus pickup
@@ -46,10 +47,10 @@ namespace HQRDa
         //      delayed - to do: Panel m_moduleStats_Display;
         //      delayed (need sprites) - to do: Sprite m_shipSprite;
         //      delayed (need sprites) - to do: Sprite m_moduleSprite;
-        // - Ship/Module Update Mechanics
         // - Add Highest wave achieved in SaveObject
         // - Creates bonus rarity for shields and repairDrones bonuses
         // - Remove all _lifeTime updates from Bonuses and replace with Map.IsOnScreen check (as done in repairbonus.cs)
         // - dans UI_EndGame. cs possible de caluler et afficher une mention a partir des loot et du score? (: viur 'NOTE1' dans UI_EndGame.cs)
     }
 }
+
