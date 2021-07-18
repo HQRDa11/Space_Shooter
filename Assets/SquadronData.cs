4cf49dd678131eb1b9b391ffcb4d2e351da465eb
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-18 03:46:45 +0200

    Update SquadronData.cs

--- Assets/SquadronData.cs
+++ Assets/SquadronData.cs
@@ -2,121 +2,142 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface ILoadableDataElement<T>
-{
-    public T Load();
-}
-
+public enum ModuleType { SHIELD = 0, REPAIRDRONE }
+// SQUADRONDATA // has 1-* MEMBERDATAS
 [System.Serializable]
-public class SquadronData 
+public class SquadronData
 {
-    [SerializeField]
-    private PlayerData m_player;
-    public PlayerData Player { get => m_player; }
+    // SQUAD MEMBERS:
+
     [SerializeField]
     private int m_maxAllies;
 
-    [SerializeField]
-    private ShipData[] m_allShips;
+    [SerializeReference] // [SerializeReference] is MonoBeahaviour feature to support serialized field inheritance ( using pattern decorator )
+    private PlayerData m_player;
+    public PlayerData Player { get => m_player; }
 
     [SerializeField]
-    private AllyData[] m_allies;
+    private MemberData[] m_allMembers;
+    public MemberData[] AllMembers { get => m_allMembers; }
+
+    // STORED OBJECTS:
+    [SerializeField]
+    private ModuleData[] m_allStoredModules;
+    public ModuleData[] AllStoredModules { get => m_allStoredModules; }
 
     [SerializeField]
-    private ModuleData[] m_allModules;
-    
-    public SquadronData()
+    private ShipData[] m_allStoredShips;
+    public ShipData[] AllStoredShips { get => m_allStoredShips; }
+
+    public SquadronData(string PlayerName)
     {
-        m_player = new PlayerData("PLAYER");
+        m_player = new PlayerData(PlayerName);
         m_maxAllies = 1;
-        m_allShips = null;
-        m_allies = new AllyData[1];
-        m_allies[0] = new AllyData("ALLY ONE");
+        m_allStoredShips = null;
+        m_allMembers = new MemberData[2];
+        m_allMembers[0] = m_player;
+        m_allMembers[1] = new MemberData("ALLY ONE");
+        m_allStoredModules = new ModuleData[0];
     }
 }
 
-[System.Serializable]
-public class PlayerData
+// MEMBERDATA 
+[System.Serializable] // Player data is a MemberData
+public class PlayerData : MemberData
+{
+    // Here we'll stock some more player specific datas
+    public PlayerData(string name) : base (name)
+    {
+        
+    }
+} 
+
+[System.Serializable] // Has 1 ShipData
+public class MemberData
 {
     [SerializeField]
     private string m_name;
-    public string name { get => m_name; }
+    public string Name { get { return m_name; } set { m_name = value; } }
     [SerializeField]
     private ShipData m_equippedShip;
-
-    public PlayerData(string name)
+    public ShipData Ship { get => m_equippedShip; }
+    public MemberData(string name)
     {
         m_name = name;
         m_equippedShip = new ShipData("LIGHT FIGHTER", new LevelData(1, 0, 100));
     }
 }
-[System.Serializable]
-public class AllyData
-{
-    [SerializeField]
-    private string m_name;
-    [SerializeField]
-    private ShipData m_equippedShip;
-    public AllyData(string name)
-    {
-        m_name = name;
-        m_equippedShip = new ShipData("LIGHT FIGHTER", new LevelData(1, 0, 100));
-    }
-}
-[System.Serializable]
+
+//SHIPDATADATA
+[System.Serializable] // has 0-* ModuleDatas // has a LevelData
 public class ShipData
 {
     [SerializeField]
     private string m_name;
+    public string Name { get => m_name; }
     [SerializeField]
-    private LevelData m_Level;
-    [SerializeField]
-    private ModuleData[] m_equippedModules;
+    private LevelData m_level;
+    public LevelData Level { get => m_level; }
 
-    public ShipData(string name,LevelData lvlData)
+    [SerializeField]
+    private ModuleData[] m_allModules;
+    public ModuleData[] AllModules { get => m_allModules; }
+
+    public ShipData(string name, LevelData lvlData)
     {
         m_name = name;
-        m_Level = new LevelData(1, 0, 100);
-        m_equippedModules = new ModuleData[3];
-        m_equippedModules[0] = new ModuleData(ModuleType.SHIELD, Rarity.GREY, "", new LevelData(1, 0, 100));
+        m_level = lvlData;
+        m_allModules = new ModuleData[3];
+        m_allModules[0] = new ModuleData(ModuleType.SHIELD, Rarity.GREY, "", new LevelData(1, 0, 100));
     }
 }
 
-public enum ModuleType { SHIELD = 0, REPAIRDRONE }
-[System.Serializable]
+//MODULEDATA
+[System.Serializable] // has 1 LevelData
 public class ModuleData
 {
+
     [SerializeField]
     private ModuleType m_type;
+    public ModuleType Type { get => m_type; }
     [SerializeField]
     private Rarity m_rarity;
+    public Rarity Rarity { get => m_rarity; }  
     [SerializeField]
     private string m_name;
+    public string Name { get => m_name; }
+
     [SerializeField]
-    private LevelData m_Level;
+    private LevelData m_level;
+    public LevelData Level { get => m_level; }
 
     public ModuleData(ModuleType type, Rarity rarity, string name, LevelData levelData)
     {
         m_type = type;
         m_rarity = rarity;
-        m_Level = levelData;
+        m_level = levelData;
         m_name = rarity + " " + type + name;
     }
 }
+
+//LEVELDATA 
 [System.Serializable]
 public class LevelData
 {
     [SerializeField]
     private int m_current;
+    public int Current { get => m_current; }
     [SerializeField]
     private float m_xp_current;
+    public float Xp_Current { get => m_xp_current; }
     [SerializeField]
     private float m_xp_toNextLevel;
+    public float Xp_ToNextLevel{ get => m_xp_toNextLevel; }
 
     public LevelData(int current, float currentXp, float nextLvLXp)
     {
         m_current = current;
-        m_xp_current = current;
+        m_xp_current = currentXp;
         m_xp_toNextLevel = nextLvLXp;
     }
 }
