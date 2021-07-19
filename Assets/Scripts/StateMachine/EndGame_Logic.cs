c8815f8413897f8c433f30c503d3908625e3d18a
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-19 10:15:46 +0200

    Create EndGame_Logic.cs

---
+++ Assets/Scripts/StateMachine/EndGame_Logic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame_Logic
+{
+    private GameInfo m_gameInfo;
+    private FinalLoot m_finalLoot;
+    public FinalLoot FinalLoot { get => m_finalLoot; }
+    public GameInfo GameInfo { get => m_gameInfo; }
+    public EndGame_Logic()
+    {
+        m_gameInfo = GameObject.Find("GameInfo").GetComponent<GameInfo>();
+        if (m_gameInfo == null)
+        {
+            Debug.LogWarning(" No GameInfo Object");
+        }
+        GameObject.Find("ProfileHandler").GetComponent<ProfileHandler>().UpdateProfile_WithGameResults(m_gameInfo);
+        m_finalLoot = new FinalLoot(1, GameInfo.Get_Score());
+    }
+
+    public void OnUserChoice(int choice)
+    {
+        Debug.LogWarning("index " + choice + " on tab length" + m_finalLoot.Options.Length);
+        Application_StateMachine.Instance.Get_CurrentState().WaitUserEntry();
+        ProfileHandler.Instance.ActiveProfile.SquadronData.AddModule(m_finalLoot.Options[choice]);
+        ProfileHandler.Instance.StateSave();
+    }
+}
