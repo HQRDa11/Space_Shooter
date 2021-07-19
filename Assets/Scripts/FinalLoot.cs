e85c79b8fc4fc655b0f73c604eea9e5658578125
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-19 04:08:52 +0200

    Create FinalLoot.cs

---
+++ Assets/Scripts/FinalLoot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalLoot 
+{
+    private ModuleData[] m_options;
+    public ModuleData[] Options  { get => m_options; }
+
+    public FinalLoot(int level, int score)
+    {
+        m_options = new ModuleData[3];
+        for (int i = 0; i<m_options.Length; i++)
+        {
+            m_options[i] = Factory.Instance.Module_Factory.Dice_Module(level);
+        }
+    }
+}
