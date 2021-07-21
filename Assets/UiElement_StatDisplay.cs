aaf65496679c40335d6c693f68e708b94d0bd074
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-21 11:13:17 +0200

    Create UiElement_StatDisplay.cs

---
+++ Assets/UiElement_StatDisplay.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiElement_StatDisplay : MonoBehaviour
+{
+    // Only used to find object in prefab
+}
