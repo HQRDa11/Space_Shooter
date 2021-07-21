149a0071acb90a041dd7358a263f0d358c2cff49
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-21 16:55:22 +0200

    BugFix

--- Assets/UiElement_StatDisplay.cs
+++ Assets/UiElement_StatDisplay.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class UiElement_StatDisplay : MonoBehaviour
 {
+    private void Start()
+    {
+        if (this.transform.parent == null)
+        {
+            Debug.Log("No parent");
+            switch (gameObject.transform.parent==null)
+            {
+                case true:
+                    this.gameObject.transform.SetParent(GameObject.FindObjectOfType<UiElement_VerticalGridLayout>().transform);
+                    break;
+            }
+        }
+    }
     // Only used to find object in prefab
 }
