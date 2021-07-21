903c9ac0795be5631badd88b2c987b49f2632cf4
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-21 11:10:36 +0200

    Create ModuleStat_Factory.cs

---
+++ Assets/ModuleStat_Factory.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleStat_Factory 
+{
+    public ModuleStat_Factory()
+    {
+
+    }
+
+    public ModuleStat[] Create_Stats(ModuleData data)
+    {
+        float levelMax = 3 * (((int)data.Rarity) + 1);
+        //double increment = Math.Log(level + 1);
+
+        float tierBaseValue = Mathf.Log(data.Tier+(int)data.Rarity);
+        float maxValue = tierBaseValue * levelMax;
+
+        float currentValue = data.Level.Current * maxValue / levelMax;
+
+
+        Debug.LogWarning("NEW TEST:" + tierBaseValue);
+        switch (data.Type)
+        {
+            case ModuleType.REPAIRDRONE:
+
+                ModuleStat[] repairStats = new ModuleStat[2];
+
+                float lifeRatio = 4;
+                float lifeSpan = currentValue / lifeRatio;
+                repairStats[0] = new ModuleStat( " LifeSpan: ", lifeSpan,maxValue/lifeRatio);
+
+                float efficiencyRatio = 12;
+                float efficiency = currentValue / efficiencyRatio;
+                repairStats[1] = new ModuleStat(" Efficiency: ", efficiency, maxValue / efficiencyRatio);
+
+                return repairStats;
+
+            case ModuleType.SHIELD:
+
+                ModuleStat[] shieldStat = new ModuleStat[1];
+
+                float energyRatio = 2;
+                float energy = currentValue / energyRatio;
+                shieldStat[0] = new ModuleStat(" Energy: ", energy, maxValue / energyRatio);
+
+                return shieldStat;
+
+            case ModuleType.TURRET:
+
+                ModuleStat[] turretStat = new ModuleStat[1];
+
+                float damageRatio = 2;
+                float damage = currentValue / damageRatio;
+                turretStat[0] = new ModuleStat(" Energy: ", damage, maxValue / damageRatio);
+
+                return turretStat;
+        }
+        return null;
+    }
+}
