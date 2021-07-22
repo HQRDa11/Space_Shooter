345de0d35903cb6f2831c9e340615009a8ed837a
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-22 17:15:50 +0200

    Revert "Revert "Merge branch 'HQRDà' into main""
    This reverts commit 506b06c729b252dc0f46e7f0f9b2137d94637a35.

--- Assets/Shield.cs
+++ Assets/Shield.cs
@@ -7,15 +7,21 @@ public class Shield : MonoBehaviour
     private float m_energy;
     public float Energy { get => m_energy; }
     private float m_maxEnergy;
-
+    private int m_baseEnergy;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        m_maxEnergy = 30;
+        m_baseEnergy = 30;
+        m_maxEnergy = m_baseEnergy;
         m_energy = m_maxEnergy;
     }
 
+    public void Initialise(float energyModuleModifier)
+    {
+        m_maxEnergy = m_baseEnergy + energyModuleModifier;
+        m_energy = m_maxEnergy;
+    }
     // Update is called once per frame
     void Update()
     {
