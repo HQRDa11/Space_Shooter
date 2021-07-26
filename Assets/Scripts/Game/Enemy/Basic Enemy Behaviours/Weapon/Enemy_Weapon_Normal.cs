35984fcb95df7672ab9be2676c58d00b270e54a8
NQSH <76408315+NQSH@users.noreply.github.com>
2021-07-26 19:32:00 +0200

    Update Enemy_Weapon_Normal.cs

--- Assets/Scripts/Game/Enemy/Basic Enemy Behaviours/Weapon/Enemy_Weapon_Normal.cs
+++ Assets/Scripts/Game/Enemy/Basic Enemy Behaviours/Weapon/Enemy_Weapon_Normal.cs
@@ -6,7 +6,8 @@ public class Enemy_Weapon_Normal : Enemy_Behaviours.Weapon
 {
     public void Shoot(Enemy enemy)
     {
-        GameObject shot = Factory.Instance.Shot_Factory.Create_DefaultEnemyShot(enemy.transform.rotation* Vector2.up);
+        GameObject shot = Factory.Instance.Shot_Factory.Create_DefaultEnemyShot(enemy.transform.rotation * Vector2.up);
+
         shot.transform.position = enemy.transform.position;
     }
     public void ShootOverTime(Enemy enemy) { }
