78720249f943756f60291ff8927e7837b9a817e6
NQSH <76408315+NQSH@users.noreply.github.com>
2021-07-27 10:21:35 +0200

    Sprite + Meta

--- Assets/Library.cs
+++ Assets/Library.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Library
+namespace Library
 {
     public static class CheckPoints
     {
@@ -31,5 +31,50 @@ public static class Library
     public static class WaveList
     {
 
+    }
+
+    public static class EnemyData
+    {
+        public const int NUMBER_OF_BASIC = 3;
+        public const int NUMBER_OF_BOSS = 1;
+        public static Enemy_Data GetData(int type, int index)
+        {
+            switch (type)
+            {
+                case 1:
+                    switch (index)
+                    {
+                        case 0: return new Enemy_Data_Sample();
+                        case 1: return new Enemy_Data_Basic_NormalShot();
+                        case 2: return new Enemy_Data_Basic_CircleShot_10();
+                        case 3: return new Enemy_Data_Basic_SpiralShot_36();
+                        default:
+                            Debug.Log("Unknow Basic Enemy Index");
+                            return null;
+                    }
+
+                case 2:
+                    switch (index)
+                    {
+                        case 0: return new Enemy_Data_Sample();
+                        case 1: return new Enemy_Data_TheWorm_Head();
+                        default:
+                            Debug.Log("Unknow Boss Enemy Head Index");
+                            return null;
+                    }
+                case 3:
+                    switch (index)
+                    {
+                        case 0: return new Enemy_Data_Sample();
+                        case 1: return new Enemy_Data_TheWorm_Body();
+                        default:
+                            Debug.Log("Unknow Boss Enemy Body Index");
+                            return null;
+                    }
+                default: Debug.Log("Unknow Enemy Type Index"); break;
+            }
+
+            return null;
+        }
     }
 }
