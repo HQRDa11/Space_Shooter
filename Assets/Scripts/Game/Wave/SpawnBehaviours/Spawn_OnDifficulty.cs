1be8a9b3d01d3c1f0ab1bac13015edf74a84eadf
NQSH <76408315+NQSH@users.noreply.github.com>
2021-07-27 10:18:26 +0200

    Create Spawn_OnDifficulty.cs

---
+++ Assets/Scripts/Game/Wave/SpawnBehaviours/Spawn_OnDifficulty.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_OnDifficulty : SpawnBehaviour
+{
+    public void Spawn(Wave wave)
+    {
+        for (int i = 1; i <= Library.EnemyData.NUMBER_OF_BASIC; i++)
+        {
+            if (i == 1)
+            {
+                GameObject gameObject1 = Factory.Instance.Enemy_Factory.CreateEnemy(wave.SpawnPoint, wave.AllEnemies.Count + 1, wave, wave.EnemyInstanceClock, ((int)EnemyType.Basic), i);
+                wave.AllEnemies.Add(gameObject1);
+                wave.EnemyRemaining--;
+
+                if (wave.Mirror)
+                {
+                    GameObject gameObject2 = Factory.Instance.Enemy_Factory.CreateEnemy(wave.SpawnPoint * new Vector2(-1, 1), wave.AllEnemies.Count + 1, wave, wave.EnemyInstanceClock, ((int)EnemyType.Basic), i);
+                    gameObject2.GetComponent<Enemy>().SetMovementBehaviour(new Enemy_Movement_MoveToCheckPoints_Mirror());
+                    wave.AllEnemies.Add(gameObject2);
+                    wave.EnemyRemaining--;
+                }
+            }
+            else if (Random.Range(0, 100) < 100 / i * WaveSystem.Instance.CurrentWaveIndex / 100)
+            {
+                GameObject gameObject3 = Factory.Instance.Enemy_Factory.CreateEnemy(wave.SpawnPoint, wave.AllEnemies.Count + 1, wave, wave.EnemyInstanceClock, ((int)EnemyType.Basic), i);
+                wave.AllEnemies.Add(gameObject3);
+                wave.EnemyRemaining--;
+            }
+        }
+    }
+}
