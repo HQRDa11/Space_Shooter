9cbc0e59f3b0134252765b861f7288b521618702
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-28 22:23:17 +0200

    Add vibrations on enemy destruction

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -125,6 +125,7 @@ public class Enemy : MonoBehaviour
     public void OnDestruction()
     {
         _deathBehaviour.OnDestruction(this);
+        Handheld.Vibrate();
     }
     public void CanMove(bool b)
     {
