d377d4fa2abbd2d77419f1e1a2b6a86f0bd4139f
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-07-30 00:05:48 +0200

    Rename Level class to CampaignLevel

--- Assets/Scripts/Game/CampaignLevel.cs
+++ Assets/Scripts/Game/CampaignLevel.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public enum Diffulty { EASY = 1, NORMAL, HARD, HARDCORE, INSANE, GODLIKE, FREE_BRUTALITY }
 
-public class Level 
+public class CampaignLevel 
 {
     private int _level;
     private int _difficulty;
     private GameObject _bossPrefab;
 
-    public Level(int level, Diffulty difficulty, GameObject bossPrefab)
+    public CampaignLevel(int level, Diffulty difficulty, GameObject bossPrefab)
     {
         _level = level;
         _difficulty = (int)difficulty;
